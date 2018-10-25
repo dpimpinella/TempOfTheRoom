0589628dfeb562bb484434a2c3ffda2b8ce0eb65
Dominic Pimpinella <dom.pimpinella@gmail.com>
2018-10-25 14:12:35 -0400

    Added comments, TODOs

--- tempoftheroom/tweet_sentiment/views.py
+++ tempoftheroom/tweet_sentiment/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
-from django.views.generic.edit import View
 from modules.tweet_sentiment import OAuthToken
 from modules.tweet_sentiment import TwitterSearch
 from modules.tweet_sentiment import SentimentAnalysis
 from .forms import QueryForm
-
-# Create your views here.
 
 class Index(FormView):
     
@@ -33,7 +29,6 @@ class Results(TemplateView):
         tweet_text_list = self.send_twitter_search(query)
         results_list = SentimentAnalysis(tweet_text_list).sentiment_results
         results = list(zip(tweet_text_list,results_list))
-        print(results)
         context = {'query':query, 'results':results}
         return render(request, 'tweet_sentiment/results.html',  context)
     
